520cfd68cf47a7a75844e3d0feac224a0dc14a05
ykttdn <38461803+ykttdn@users.noreply.github.com>
2025-09-15 22:33:34 +0900

    Allow multiple args

--- hello/main.go
+++ hello/main.go
@@ -1,11 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
 
-func sayHello() string {
-	return "Hello, World!"
+func hello(w io.Writer, args []string) {
+	if len(args) > 1 {
+		fmt.Fprintf(w, "Hello, %s!\n", strings.Join(args[1:], " "))
+	} else {
+		fmt.Fprintln(w, "Hello, World!")
+	}
 }
 
 func main() {
-	fmt.Println(sayHello())
+	hello(os.Stdout, os.Args)
 }
--- hello/main_test.go
+++ hello/main_test.go
@@ -1,14 +1,41 @@
 package main
 
 import (
+	"bytes"
 	"testing"
 )
 
-func TestSayHello(t *testing.T) {
-	want := "Hello, World!"
-	got := sayHello()
+func TestHello(t *testing.T) {
+	tests := []struct {
+		name     string
+		args     []string
+		expected string
+	}{
+		{
+			name:     "With one argument",
+			args:     []string{"program_name", "Go"},
+			expected: "Hello, Go!\n",
+		},
+		{
+			name:     "With two arguments",
+			args:     []string{"program_name", "Go", "Developer"},
+			expected: "Hello, Go Developer!\n",
+		},
+		{
+			name:     "Without argument",
+			args:     []string{"program_name"},
+			expected: "Hello, World!\n",
+		},
+	}
 
-	if got != want {
-		t.Errorf("sayHello() = %q, want %q", got, want)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var buf bytes.Buffer
+			hello(&buf, tt.args)
+
+			if buf.String() != tt.expected {
+				t.Errorf("expected: %q, actual: %q", tt.expected, buf.String())
+			}
+		})
 	}
 }
